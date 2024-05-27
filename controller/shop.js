1bfa7d9199b8d34f3980ab139dba40624612afe9
clareenz <148300455+clareenz@users.noreply.github.com>
2024-05-27 15:25:23 +0800

    reverse

--- controller/shop.js
+++ controller/shop.js
@@ -174,6 +174,7 @@ router.get(
       res.cookie("seller_token", null, {
         expires: new Date(Date.now()),
         httpOnly: true,
+        sameSite: "none",
         secure: true,
       });
       res.status(201).json({
