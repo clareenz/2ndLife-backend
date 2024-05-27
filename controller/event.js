4838d2ca750687e8a2e16239f64d40086bd51c88
clareenz <148300455+clareenz@users.noreply.github.com>
2024-05-27 14:02:40 +0800

    backend

---
+++ controller/event.js
@@ -0,0 +1,162 @@
+const express = require("express");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const { upload } = require("../multer");
+const Shop = require("../model/shop");
+const Event = require("../model/event");
+const ErrorHandler = require("../utils/ErrorHandler");
+const { isSeller, isAdmin, isAuthenticated } = require("../middleware/auth");
+const router = express.Router();
+const fs = require("fs");
+
+// create event
+router.post(
+  "/create-event",
+  upload.array("images"),
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const shopId = req.body.shopId;
+      const shop = await Shop.findById(shopId);
+      if (!shop) {
+        return next(new ErrorHandler("Shop Id is invalid!", 400));
+      } else {
+        const files = req.files;
+        const imageUrls = files.map((file) => `${file.filename}`);
+
+        const eventData = req.body;
+        eventData.images = imageUrls;
+        eventData.shop = shop;
+
+        const product = await Event.create(eventData);
+
+        res.status(201).json({
+          success: true,
+          product,
+        });
+      }
+    } catch (error) {
+      return next(new ErrorHandler(error, 400));
+    }
+  })
+);
+
+// get all events
+router.get("/get-all-events", async (req, res, next) => {
+  try {
+    const events = await Event.find();
+    res.status(201).json({
+      success: true,
+      events,
+    });
+  } catch (error) {
+    return next(new ErrorHandler(error, 400));
+  }
+});
+
+// get all events of a shop
+router.get(
+  "/get-all-events/:id",
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const events = await Event.find({ shopId: req.params.id });
+
+      res.status(201).json({
+        success: true,
+        events,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error, 400));
+    }
+  })
+);
+
+// delete event of a shop
+router.delete(
+  "/delete-shop-event/:id",
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const productId = req.params.id;
+
+      const eventData = await Event.findById(productId);
+
+      eventData.images.forEach((imageUrl) => {
+        const filename = imageUrl;
+        const filePath = `uploads/${filename}`;
+
+        fs.unlink(filePath, (err) => {
+          if (err) {
+            console.log(err);
+          }
+        });
+      });
+
+      const event = await Event.findByIdAndDelete(productId);
+
+      if (!event) {
+        return next(new ErrorHandler("Event not found with this id!", 500));
+      }
+
+      res.status(201).json({
+        success: true,
+        message: "Event Deleted successfully!",
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error, 400));
+    }
+  })
+);
+
+// Update event
+router.put(
+  "/update-event/:id",
+  isSeller,
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const eventId = req.params.id;
+      const { name, price, stock } = req.body; // Assuming these are the fields you want to update
+
+      // Validate if the event exists
+      const event = await Event.findById(eventId);
+      if (!event) {
+        return next(new ErrorHandler("Event not found with this id!", 404));
+      }
+
+      // Update the event fields
+      event.name = name;
+      event.discountPrice = price;
+      event.stock = stock;
+
+      // Save the updated event
+      await event.save();
+
+      res.status(200).json({
+        success: true,
+        event,
+        message: "Event updated successfully!",
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error, 400));
+    }
+  })
+);
+
+// all events --- for admin
+router.get(
+  "/admin-all-events",
+  isAuthenticated,
+  isAdmin("Admin"),
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const events = await Event.find().sort({
+        createdAt: -1,
+      });
+      res.status(201).json({
+        success: true,
+        events,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  })
+);
+
+module.exports = router;
